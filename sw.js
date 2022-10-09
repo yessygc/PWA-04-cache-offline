c2f8727b99cf129e65b6d97fd6fe39cf18434379
yessygc <yessgarciacampos@gmail.com>
2022-10-08 20:49:10 -0500

    Iniciando proyecto y respuesta offline básica

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
 
 
 
+self.addEventListener('fetch', event => {
+
+    const offlineResp = new Response(`
+    
+        b   ienvenido a mi página web
+
+        Disculpa, para usarla necesitas internet
+
+    `);
+
+    const resp = fetch(event.request)
+                    .catch( () =>  offlineResp );
+
+
+
+    event.respondWith( resp );
+
+});
