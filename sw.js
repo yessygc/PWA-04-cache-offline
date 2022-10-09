ce1e811c32a9ad40c4b1437b16f95f76f04f2de3
yessygc <yessgarciacampos@gmail.com>
2022-10-08 21:08:53 -0500

    Respuesta offline HTML String

--- sw.js
+++ sw.js
@@ -3,17 +3,44 @@
 
 self.addEventListener('fetch', event => {
 
-    const offlineResp = new Response(`
+    // const offlineResp = new Response(`
     
-        b   ienvenido a mi página web
+    //    b   ienvenido a mi página web
 
-        Disculpa, para usarla necesitas internet
+    //    Disculpa, para usarla necesitas internet
 
-    `);
+    //`);
+
+    // const offlineResp = new Response (`
+    
+    // <!DOCTYPE html>
+    // <html lang="en">
+    // <head>
+    //     <meta charset="UTF-8">
+    //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    //     <title>Mi PWA</title>
+
+    // </head>
+    // <body class="container p-3">
+
+    // <h1>Offline Mode</h1>
+
+    // </body>
+    // </html>
+    
+    // `, {
+    //     headers: {
+    //         'Content-Type':'text/html'
+    //     }
+    // });
+
+
+    const OfflineResp = fetch( 'pages/offline.html' );
+
 
     const resp = fetch(event.request)
-                    .catch( () =>  offlineResp );
-
+                    .catch( () => OfflineResp );
 
 
     event.respondWith( resp );
